bac1909a8276c32787a19bd6cded86949c85550a
Jonas Bandi <dev@jonasbandi.net>
2019-03-03 22:10:47 +0100

    Jokes App working

--- jokes/src/App.tsx
+++ jokes/src/App.tsx
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as queryString from 'querystring';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class App extends Component<{}, { joke: string }> {
+
+    state = {
+        joke: '...'
+    };
+
+    componentDidMount(): void {
+        fetch("https://icanhazdadjoke.com/", {headers: {Accept: "application/json"}})
+            .then((res) => res.json())
+            .then((res) => {
+                this.setState({joke: res.joke})
+            });
+    }
+
+    render() {
+
+        const parsedHash = queryString.parse(location.search.slice(1));
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Hello, {parsedHash.contactFullName}
+                    </p>
+                    <p> Here is your personal joke:</p>
+                    <p>
+                        {this.state.joke}
+                    </p>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
